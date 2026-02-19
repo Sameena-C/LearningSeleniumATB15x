7913a1775786a3cd52c234c043e0f6106085be11
Sameena-C <sameena.c@gmail.com>
2026-02-19 08:00:34 +0530

    18Feb2026

---
+++ src/test/java/systemscenarios/Selenium001.java
@@ -0,0 +1,16 @@
+package systemscenarios;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium001 {
+    public static void main(String[] args) {
+
+        // Source Code here ->
+        // Converted -> JSON Wire Protocol | HTTP Request | API request
+        // Browser Driver | Server (application running on a particular port )
+        // Browser Driver -> Real Driver
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com");
+    }
+}
